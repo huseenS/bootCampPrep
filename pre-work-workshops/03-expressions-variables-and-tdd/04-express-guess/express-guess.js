65698682af3064c73f25d62c7927622855ead4c1
huseenS <huseensufi@gmail.com>
2018-07-05 13:46:06 -0600

    initial commit

---
+++ pre-work-workshops/03-expressions-variables-and-tdd/04-express-guess/express-guess.js
@@ -0,0 +1,2 @@
+// YOUR CODE BELOW
+var myGuess = 20 * (50 / (5 * 2) + 15);
---
+++ pre-work-workshops/04-control-flow/01-founder-finder/founder-finder.js
@@ -0,0 +1,14 @@
+let name = 'David';
+let name = 'Nimit';
+// let name = 'Someone else';
+
+let found;
+let found = false;
+
+if (name === "David") {
+	found = true;
+} else if(name === Nimit) {
+	found = true;
+}
+
+// YOUR CODE BELOW
